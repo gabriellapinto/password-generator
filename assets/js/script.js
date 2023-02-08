24ed3c21f2fec768d461bd749100891c5c69a235
Gabby <gabbympinto@gmail.com>
2023-02-08 18:34:29 -0500

    added prompts and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Prompts to get user input
+var passLength = prompt("How long would you like your password?");
+var lowercase = confirm("Would you like your password to include lowercase characters?");
+var uppercase = confirm("Would you like your password to include uppercase characters?");
+var numeric = confirm("Would you like your password to include numbers?");
+var specialChar = confirm("Would you like your password to include special characters?");
+
+// Characters for password
+
+const alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const uniqueChar = ["!", """, "#", "$", "%", "&", ",'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+// Generate Password Function
+function generatePassword() {
+
+  
+
+
+
+
+
+}
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
